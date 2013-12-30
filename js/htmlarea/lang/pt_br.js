e85c768a5a189e125aefa8739ea5942fb49c5b32
Mauricio Luiz Viani <viani@celepar.pr.gov.br>
2013-12-30 17:06:35 -0200

    #1 - Commit inicial do módulo Workflow para o ExpressoLivre

---
+++ js/htmlarea/lang/pt_br.js
@@ -0,0 +1,59 @@
+// I18N constants
+
+HTMLArea.I18N = {
+
+	// the following should be the filename without .js extension
+	// it will be used for automatically load plugin language.
+	lang: "pt_br",
+
+	tooltips: {
+		bold:           "Negrito",
+		italic:         "Itálico",
+		underline:      "Sublinhado",
+		strikethrough:  "Riscado",
+		subscript:      "Subescrito",
+		superscript:    "Sobrescrito",
+		justifyleft:    "Alinhar à esquerda",
+		justifycenter:  "Centralizar",
+		justifyright:   "Alinhar à direita",
+		justifyfull:    "Justificar",
+		orderedlist:    "Lista com Números",
+		unorderedlist:  "Lista com Marcadores",
+		outdent:        "Deslocar texto para esquerda",
+		indent:         "Deslocar texto para direita",
+		forecolor:      "Cor de Texto",
+		hilitecolor:    "Cor de Fundo",
+		inserthorizontalrule: "Linha Horizontal",
+		createlink:     "Inserir link WEB",
+		insertimage:    "Inserir / Modificar Imagem",
+		inserttable:    "Inserir Tabela",
+		htmlmode:       "Ver Documento em HTML",
+		popupeditor:    "Aumentar Editor",
+		about:          "Sobre este Editor",
+		showhelp:       "Ajuda",
+		textindicator:  "Estilo Atual",
+		undo:           "Desfazer",
+		redo:           "Refazer",
+		cut:            "Recortar",
+		copy:           "Copiar",
+		paste:          "Colar",
+		lefttoright: 	"Direção do texto : esquerda para direita",
+		righttoleft:	"Direção do texto : direita para esquerda"
+		
+	},
+
+	buttons: {
+		"ok":           "Aceitar",
+		"cancel":       "Cancelar"
+	},
+
+	msg: {
+		"Path":          "Caminho",
+		"TEXT_MODE":     "Está em modo TEXTO. Use o botão [HTML] acima, para ativar modo HTML.",
+		"Moz-Clipboard": "A funções de Cortar/Copiar/Colar no formato RTF (Rich Text Format) " +
+						"foram desabilitadas pelo Browser Mozilla/Firefox, por razões de segurança."+
+					"Clique no botão OK para ver como habilitar essas funções no seu Browser Mozilla/Firefox " +
+					" (instruções do próprio site mozilla.org).\nImportante:Se você liberar,será permitido acessar "+
+					" a Área de Transferência via Script!"
+	}
+};
